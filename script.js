bc189b5c1ece79c09892c6af400bfd2459340003
Diwash-Niroula <dniroula10@gmail.com>
2024-12-03 21:17:52 -0500

    Add API Keys and global variables for weather and Unsplash APIs

---
+++ script.js
@@ -0,0 +1,4 @@
+const weatherApiKey = '268630d05214024a5ddb5c07cad98695'; // Replace with your OpenWeatherMap API key
+const unsplashAccessKey = 'qjN5jEPoc2JDa1pPQVH2r2V1Y8qDQ3JXj--eY1kZUbs'; // Replace with your Unsplash API Access Key
+const weatherDiv = document.getElementById('weather');
+let favoriteCities = [];
